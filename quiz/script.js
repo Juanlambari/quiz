7739fcc82eb7ca2160b5f2e08c04890c7de8c57b
Juan <juanlambari1201@gmail.com>
2025-04-23 21:27:36 -0300

    timer ok

--- quiz/script.js
+++ quiz/script.js
@@ -1,2 +1,23 @@
-const input = document.getElementById("input");
-    input.value = "Texto inserido via Javascript";
+
+document.querySelector('.custom-btn').addEventListener('click', () => {
+
+let duration = 60
+
+display = document.getElementById('timer');
+timer(duration, display);
+})
+
+const timer = (duration, display) => {
+    let timer = duration;
+    setInterval(() =>{
+        seconds = timer;
+        display.innerHTML = `${timer}`
+        timer = timer < 10 ? '0' + timer : timer;
+
+        timer -= 1;
+        if(timer < 0){
+            display.innerHTML = 'ACABOU!!!';
+        }
+
+    }, 1000)
+}
